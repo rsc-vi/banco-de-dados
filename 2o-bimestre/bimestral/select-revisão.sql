255251b009fa508fc232b64b174daee599047eda
Caetano <130772629+rsc-vi@users.noreply.github.com>
2023-07-27 12:08:44 -0300

    Add files via upload

---
+++ 2o-bimestre/bimestral/select-revisão.sql
@@ -0,0 +1,35 @@
+INSERT INTO categorias(nome, id_categoria) VALUES
+('Jaciane', '1');
+INSERT INTO categorias(nome, id_categoria) VALUES
+('Rebeca', '2');
+INSERT INTO categorias(nome, id_categoria) VALUES
+('Amanda', '3');
+INSERT INTO categorias(nome, id_categoria) VALUES
+('Alice', '4');
+
+INSERT INTO clientes(nome, cidade, id_cliente) VALUES
+("Jaciane", "Rio Largo", '5');
+INSERT INTO clientes(nome, cidade, id_cliente) VALUES
+("Rebeca", "Rio Largo", "7");
+INSERT INTO clientes(nome, cidade, id_cliente) VALUES
+("Amanda", "Maceió", "6");
+INSERT INTO clientes(nome, cidade, id_cliente) VALUES
+("Alice", "Rio Largo", "8");
+
+INSERT INTO produtos(nome, preço, id_produto) VALUES
+("Rebeca", "20", "9");
+INSERT INTO produtos(nome, preço, id_produto) VALUES
+("Rebeca", "35", "2");
+INSERT INTO produtos(nome, preço, id_produto) VALUES
+("Rebeca", "70", "5");
+INSERT INTO produtos(nome, preço, id_produto) VALUES
+("Rebeca", "25", "5");
+
+INSERT INTO pedidos(valor, data, id_pedido) VALUES
+("220", "03/04", "10");
+INSERT INTO pedidos(valor, data, id_pedido) VALUES
+("340", "03/10", "20");
+INSERT INTO pedidos(valor, data, id_pedido) VALUES
+("560", "20/04", "35");
+INSERT INTO pedidos(valor, data, id_pedido) VALUES
+("890", "30/12", "89");
